b8a03ad35f1992d3f353d720d2f32e0b99432eff
kevii <kevii@10.232.19.45>
2023-09-08 11:19:56 -0700

    Version inicial del proyecto

---
+++ src/mx/itson/tortilla/negocio/Operacion.java
@@ -0,0 +1,23 @@
+
+package mx.itson.tortilla.negocio;
+
+/**
+ *
+ * @author Kevin Duarte
+ */
+public class Operacion {
+    
+    public String[] separar(String oracion){
+        
+        String[] resultado = oracion.split("-");
+    
+        return resultado;
+    
+    }
+   
+    
+    
+    
+    
+
+}
